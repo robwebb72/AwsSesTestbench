f7b560ba36aaeb9f1403d1002f83a05491c20ee0
Rob Webb <rob.webb@loyaltylogistix.com>
2023-10-19 14:30:22 +0100

    Added functionality to GetTemplatesAsync

--- AwsSesTestbench/Services/AwsTemplateService.cs
+++ AwsSesTestbench/Services/AwsTemplateService.cs
@@ -1,4 +1,9 @@
-﻿using AwsSesTestbench.Models;
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using AwsSesTestbench.AWS;
+using AwsSesTestbench.Models;
 using AwsSesTestbench.Models.AwsTemplates;
 
 namespace AwsSesTestbench.Services;
@@ -7,10 +12,27 @@ public class AwsTemplateService
 {
     public async Task<List<AwsTemplateIdModel>> GetTemplatesAsync(CancellationToken cancellationToken)
     {
-        await Task.CompletedTask;
-        return new List<AwsTemplateIdModel>();
+        var snsCredentials = new BasicAWSCredentials(AwsKeys.Access, AwsKeys.SecretAccess);
+        using var sesClient = new AmazonSimpleEmailServiceClient(snsCredentials, RegionEndpoint.EUWest1);
+
+        var listTemplatesRequest = new ListTemplatesRequest
+        {
+            MaxItems = 100
+        };
+        var response = await sesClient.ListTemplatesAsync(listTemplatesRequest, cancellationToken);
+
+        return response is null ? 
+            new List<AwsTemplateIdModel>() :
+            response.TemplatesMetadata.Select(MapTemplateMetadataToAwsTemplateIdModel).ToList();
     }
-    
+
+    private static AwsTemplateIdModel MapTemplateMetadataToAwsTemplateIdModel(TemplateMetadata templateMetadata)
+        => new()
+        {
+            Name = templateMetadata.Name,
+            TimeStamp = templateMetadata.CreatedTimestamp
+        };
+
     public async Task<(bool, string)> DeleteTemplateAsync(string name, CancellationToken cancellationToken)
     {
         await Task.CompletedTask;
