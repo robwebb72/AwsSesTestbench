deb0d04fbf679a7dc02803f6ebce32948cdfd605
Rob Webb <rob.webb@loyaltylogistix.com>
2023-10-19 11:12:37 +0100

    Added AwsTemplateController

---
+++ AwsSesTestbench/Controllers/AwsTemplateController.cs
@@ -0,0 +1,21 @@
+﻿using AwsSesTestbench.Models;
+using AwsSesTestbench.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwsSesTestbench.Controllers;
+
+[ApiController]
+[Route("api/template")]
+public class AwsTemplateController : ControllerBase
+{
+    private readonly AwsTemplateService _awsTemplateService;
+
+    public AwsTemplateController(AwsTemplateService awsTemplateService)
+    {
+        _awsTemplateService = awsTemplateService;
+    }
+
+    [HttpGet]
+    public async Task<List<AwsTemplateId>> GetTemplateAsync(CancellationToken cancellationToken)
+        => await _awsTemplateService.GetTemplatesAsync(cancellationToken);
+}
