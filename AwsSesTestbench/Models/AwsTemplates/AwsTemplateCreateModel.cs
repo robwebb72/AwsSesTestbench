eb2566cf3b94b3780a62ba87aaadca17187fbe3d
Rob Webb <rob.webb@loyaltylogistix.com>
2023-10-19 14:09:49 +0100

    Added AwsTemplateCreateModel

---
+++ AwsSesTestbench/Models/AwsTemplates/AwsTemplateCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AwsSesTestbench.Models.AwsTemplates;
+
+public record AwsTemplateCreateModel
+{
+    public string TemplateName { get; init; } = string.Empty;
+    public string Subject { get; init; } = string.Empty;
+    public string TextPart { get; init; } = string.Empty;
+    public string HtmlPart { get; init; } = string.Empty;
+}
