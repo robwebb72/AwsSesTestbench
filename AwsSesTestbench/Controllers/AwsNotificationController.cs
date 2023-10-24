67b457c16595c7cac818e601185450a3d6f7fe19
Rob Webb <robstah72@gmail.com>
2023-10-24 13:01:09 +0100

    Added basic AwsNotificationModel

--- AwsSesTestbench/Controllers/AwsNotificationController.cs
+++ AwsSesTestbench/Controllers/AwsNotificationController.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using AwsSesTestbench.Models;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -12,7 +14,17 @@ public class AwsNotificationController : ControllerBase
     [Consumes("text/plain")]
     public async Task<ActionResult<string>> ReceiveAwsNotification([FromBody] string notificationString, CancellationToken cancellationToken)
     {
+        var jsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var messageModel = JsonSerializer.Deserialize<AwsNotificationModel>(notificationString, jsonSerializerOptions);
         await Task.CompletedTask;
-        return Ok(notificationString);
+
+        var (success, resultString) = (messageModel?.Type) switch
+        {
+            "Notification" => (true, "handled notification"),
+            "SubscriptionConfirmation" => (true, "dealt with subscription confirmation"),
+            _ => (false, "unknown message type")
+        };
+
+        return success ? Ok(resultString) : BadRequest(resultString);
     }
 }
---
+++ AwsSesTestbench/Models/AwsNotificationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AwsSesTestbench.Models;
+
+public class AwsNotificationModel
+{
+    public string Type { get; set; }
+    public string TopicArn { get; set; }
+    public string Message { get; set; }    
+}
