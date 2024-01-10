fbea33be16d54fcdb6670fdb06b018ec075dbe5d
Rob Webb <robstah72@gmail.com>
2024-01-10 10:29:18 +0000

    Merge conflict

--- AwsSesTestbench/Setup/DbConnectionSetup.cs
+++ AwsSesTestbench/Setup/DbConnectionSetup.cs
@@ -5,11 +5,11 @@ namespace AwsSesTestbench.Setup;
 
 public static class DatabaseConnectionSetup
 {
-    const bool useRemoteDb = true;
+    const bool UseRemoteDb = false;
 
     public static void SetupDatabaseConnection(this WebApplicationBuilder builder)
     {
-        var connectionString = builder.Configuration.GetConnectionString(useRemoteDb ? "Remote": "Local");
+        var connectionString = builder.Configuration.GetConnectionString(UseRemoteDb ? "Remote": "Local");
         builder.Services.AddDbContext<AwsSesTestbenchDbContext>(opt => opt.UseSqlServer(connectionString));        
     }
 }
